7f13b5d0ba9f74c5aa310a0409bf23054efde042
nhatkent333 <89648281+nhatkent333@users.noreply.github.com>
2024-06-13 21:29:56 +0700

    Update fetch_metadata.py

--- api/fetch_metadata.py
+++ api/fetch_metadata.py
@@ -1,8 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 @app.route('/api/fetch-metadata', methods=['GET'])
 def fetch_metadata():
